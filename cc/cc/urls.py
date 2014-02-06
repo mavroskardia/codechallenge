480991fbdda863cfa558292ed17404ac1d37a675
Andy <chryso@gmail.com>
2014-02-05 22:54:19 -0500

    In progress auth pages

--- cc/cc/urls.py
+++ cc/cc/urls.py
@@ -13,8 +13,7 @@ urlpatterns = patterns('',
     url(r'^landing/', include('apps.landing.urls', namespace='landing')),
     url(r'^coder/', include('apps.coder.urls', namespace='coder')),
     url(r'^challenge/', include('apps.challenge.urls', namespace='challenge')),
+    url(r'^ccauth/', include('apps.ccauth.urls', namespace='auth')),
 
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', { 'template_name': 'coder/login.html' }, name='login'),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^admin/', include(admin.site.urls)),
 )
