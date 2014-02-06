fb77d3d244ad7bec0491e5691294b9907670d189
Andy <chryso@gmail.com>
2014-02-05 20:52:33 -0500

    Added rudimentary account handling

--- cc/cc/urls.py
+++ cc/cc/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.core.urlresolvers import reverse
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,5 +13,8 @@ urlpatterns = patterns('',
     url(r'^landing/', include('apps.landing.urls', namespace='landing')),
     url(r'^coder/', include('apps.coder.urls', namespace='coder')),
     url(r'^challenge/', include('apps.challenge.urls', namespace='challenge')),
+
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', { 'template_name': 'coder/login.html' }, name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^admin/', include(admin.site.urls)),
 )
