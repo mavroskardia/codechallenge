f96442c6a1fea4be821e3c401ef51d77eb990358
Andy <chryso@gmail.com>
2014-02-07 17:16:05 -0500

    Added more profile logic and some initial form processing

--- cc/apps/coder/views/profile.py
+++ cc/apps/coder/views/profile.py
@@ -7,14 +7,28 @@ from django.contrib import messages
 from django.core.urlresolvers import reverse
 
 from ..models import Coder
+from ..forms import ProfileForm
 
 
 class ProfileView(View):
+    form_class = ProfileForm
     ro_template_name = 'coder/ro_profile.html'
     rw_template_name = 'coder/rw_profile.html'
 
     def get(self, request, username, *args, **kwargs):
         coder = get_object_or_404(Coder, user__username=username)
-        template = self.rw_template_name if request.user == coder.user else self.ro_template_name
 
-        return render(request, template, { 'coder': coder })
+        if request.user == coder.user:
+            return render(request, self.rw_template_name, { 'coder': coder, 'form': self.form_class() })
+
+        return render(request, self.ro_template_name, { 'coder': coder })
+
+    def post(self, request, username, *args, **kwargs):
+        coder = get_object_or_404(Coder, user__username=username)
+
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            messages.info(request, 'Successfully updated profile')
+
+        return render(request, self.rw_template_name, { 'coder': coder, 'form': form })
