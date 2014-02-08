758167d97121b8bca3ff299004c9e23b666c322c
Andy <chryso@gmail.com>
2014-02-07 19:00:37 -0500

    Basic admin interface activation

--- cc/apps/coder/admin.py
+++ cc/apps/coder/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from .models import Coder, Level
 
-admin.site.register(Coder)
-admin.site.register(Level)
+
+class CoderAdmin(admin.ModelAdmin):
+	pass
+
+class LevelAdmin(admin.ModelAdmin):
+	list_display_links = ('id',)
+	list_display = ('id', 'name', 'starting_xp')
+	list_editable = ('name', 'starting_xp')
+
+
+admin.site.register(Coder, CoderAdmin)
+admin.site.register(Level, LevelAdmin)
