2f8483584bc6119309dabe9898f22620a9defb7f
Andy <chryso@gmail.com>
2014-03-07 17:32:06 -0500

    Updating styling

--- cc/apps/challenge/forms.py
+++ cc/apps/challenge/forms.py
@@ -6,12 +6,31 @@ from .models import Rule
 
 
 class ChallengeForm(ModelForm):
-	name = forms.CharField(required=True)
-	duration = forms.CharField(required=True)
 	class Meta:
 		model = Challenge
 
+	name = forms.CharField(
+		required=True,
+		widget=forms.TextInput(
+			attrs={
+			'placeholder': 'What is the name of this challenge?',
+			'maxlength': 256,
+			'size': 50,
+			'class': 'form-control'
+			}))
+
+	duration = forms.CharField(
+		required=True,
+		widget=forms.TextInput(
+			attrs={
+			'placeholder': 'How long, in days, does this Challenge last? (ex. 30)',
+			'maxlength': 3,
+			'size': 5,
+			'class': 'form-control'
+			}))
+
 class RuleForm(ModelForm):
 	description = forms.CharField(required=True)
+
 	class Meta:
 		model = Rule
