f1fa16c3049d461a6ca79ecabb9464399467bdde
Andy <chryso@gmail.com>
2014-03-11 11:12:56 -0400

    Tweaking some styling

--- cc/apps/coder/models.py
+++ cc/apps/coder/models.py
@@ -12,12 +12,14 @@ class Coder(models.Model):
 	def level(self):
 		return Level.objects.filter(starting_xp__lte=self.xp).order_by('-starting_xp').first()
 
+	def webname(self):
+		return self.name or self.user.username
+
 	def __str__(self):
 		return self.__unicode__()
 
 	def __unicode__(self):
-		name = self.name or self.user.username
-		return '{name} ({self.xp})'.format(name=name, self=self)
+		return '{name} ({self.xp})'.format(name=self.webname(), self=self)
 
 class Level(models.Model):
 	name = models.CharField(max_length=256)
