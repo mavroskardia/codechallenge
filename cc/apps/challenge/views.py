b13359e7907cc8a38013c9c82ed6cf256de80dc6
Andy <chryso@gmail.com>
2014-03-11 14:59:38 -0400

    Activated font awesome

--- cc/apps/challenge/views.py
+++ cc/apps/challenge/views.py
@@ -15,11 +15,19 @@ from .forms import ChallengeForm, AddRuleFormset, AddRuleTemplateFormset
 class IndexView(generic.ListView):
     model = Challenge
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(IndexView, self).get_context_data(*args, **kwargs)
+
+        if self.request.user.is_authenticated and not self.request.user.is_anonymous():
+            context['challenges_im_in'] = Challenge.objects.filter(participant__coder=self.request.user.coder)
+
+        return context
+
 class DetailView(generic.DetailView):
     model = Challenge
 
-    def get_context_data(self, **kwargs):
-        context = super(DetailView, self).get_context_data(**kwargs)
+    def get_context_data(self, *args, **kwargs):
+        context = super(DetailView, self).get_context_data(*args, **kwargs)
         challenge = context['object']
 
         context['owner'] = challenge.participant_set.get(is_owner=True)
