0ddfafd1d628ebb4f29ceaaf0d08ccef778a7513
Andy <chryso@gmail.com>
2014-03-21 14:51:20 -0400

    Split the two parts of the entry comments for better layout control

--- cc/apps/landing/static/js/juicy.js
+++ cc/apps/landing/static/js/juicy.js
@@ -5,9 +5,6 @@
 		var header_animator = new HeaderAnimator('binary_canvas');
 		header_animator.init();
 		header_animator.start();
-
-		var nav_handler = new NavHandler('main_nav');
-		nav_handler.init();
 	});
 
 }());
